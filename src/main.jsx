f29e1ff17ee74ec364c2c65b8c712ba67b4feeab
Thiago Freitas <thiagofreitas2909@gmail.com>
2022-10-05 18:46:44 -0300

    Config estilização global

--- src/main.jsx
+++ src/main.jsx
@@ -4,29 +4,28 @@ import App from "./App";
 
 import { Global, css } from "@emotion/react";
 
-const GlobalStyles = css`
-  @import url("https://fonts.googleapis.com/css2?family=Overpass:wght@100;200;300;400;500;600;700&family=Roboto:wght@100;300;400;500;700&display=swap");
-  * {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    list-style: none;
-    text-decoration: none;
-    font-family: "Roboto", "Overpass", sans-serif;
-  }
-
-  html,
-  body {
-    width: 100%;
-    min-height: 100vh;
-  }
-`;
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Global styles={GlobalStyles} />
+    <Global
+      styles={css`
+        @import url("https://fonts.googleapis.com/css2?family=Overpass:wght@100;200;300;400;500;600;700&family=Roboto:wght@100;300;400;500;700&display=swap");
+
+        * {
+          padding: 0;
+          margin: 0;
+          box-sizing: border-box;
+          list-style: none;
+          text-decoration: none;
+          font-family: "Roboto", "Overpass", sans-serif;
+        }
+
+        html,
+        body {
+          width: 100%;
+          min-height: 100vh;
+        }
+      `}
+    />
     <App />
   </React.StrictMode>
 );
-
-
