bf078cd6c1ef3b27e48361ce1cc377983a2ffbf6
Thiago Freitas <thiagofreitas2909@gmail.com>
2022-10-05 18:42:49 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+export default function App() {
+  return (
+    <div className="App">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+        quibusdam distinctio nihil temporibus, fuga, exercitationem ex explicabo
+        obcaecati eligendi, voluptatibus minus praesentium aliquid inventore
+        quaerat sunt ad doloremque eum quasi.
+      </p>
+    </div>
+  );
+}
+
+/* import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; */
+
+
+/*
+<Router>
+  <Header />
+  <Container>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/anime-list" element={<AnimeList />} />
+      <Route path="/manga-list" element={<MangaList />} />
+      <Route path="/waifus" element={<Waifus />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/browse" element={<Browse />} />
+      <Route path="/forum" element={<Forum />} />
+    </Routes>
+  </Container>
+  <Footer />
+</Router> 
+*/
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import { Global, css } from "@emotion/react";
+
+const GlobalStyles = css`
+  @import url("https://fonts.googleapis.com/css2?family=Overpass:wght@100;200;300;400;500;600;700&family=Roboto:wght@100;300;400;500;700&display=swap");
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+    font-family: "Roboto", "Overpass", sans-serif;
+  }
+
+  html,
+  body {
+    width: 100%;
+    min-height: 100vh;
+  }
+`;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Global styles={GlobalStyles} />
+    <App />
+  </React.StrictMode>
+);
+
+
